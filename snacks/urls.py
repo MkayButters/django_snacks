88a11b4829da93dd668c733be287af68926ff9b4
Ryan Pilon <ryanpilon1995@gmail.com>
2020-12-16 16:15:33 -0800

    finished tests/aboutviews

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, AboutPageView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path("about/", AboutView.as_view(), name="about"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
 
 
